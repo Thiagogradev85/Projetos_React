61fe0360509274e6df248a9c580843520b4917eb
Thiago Gramuglia <thiago_gra@hotmail.com>
2022-09-18 14:34:38 -0300

    ok

---
+++ react-app/src/App.js
@@ -0,0 +1,19 @@
+
+//CSS
+import './App.css';
+
+//Components
+import FirstComponent from "./components/FirstComponent"
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello Word!!</h1>
+      <FirstComponent/>
+     
+    </div>
+  );
+}
+
+export default App;
+
